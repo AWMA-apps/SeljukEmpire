1dd68f6fb6109c42b8093ee23d61ff71d561c99d
AWMA-apps <telecomandcoding@gmail.com>
2025-08-26 23:20:50 +0300

    release 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         applicationId = "com.awma.seljukempire"
         minSdk = 23
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
